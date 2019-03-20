d5d453de8d5ef9c7a0460e763f6a36656c9dc089
Kesing Huang <kesing.huang@gmail.com>
2019-03-20 22:59:04 +0100

    223 done

--- todo2/assets/js/main.js
+++ todo2/assets/js/main.js
@@ -1,22 +1,17 @@
-$(document).ready(function(){
     //check off specific item by clicking
-    $('li').on("click",function(){ 
-      //if li is gray
-        if (($(this).css) === "rgb(128, 128, 128)"){
-       //turn it black
-        //else
-          //trun it gray
-        }
-        $(this).css({
-            color: "gray",
-            textDecoration: "line-through"
-        })
+    $("li").click(function(){ 
+      $(this).toggleClass("completed");
+    })
+
+    //click on X to delet todo
+    $("span").click(function(){
+      $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+      });
+      event.stopPropagation();
     })
     
 
 
 
 
-
-    
-});
