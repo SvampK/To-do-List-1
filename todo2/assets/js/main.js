210f1fe0513c5ee190e70b8f76fa6aa8d2cb6be7
Kesing Huang <kesing.huang@gmail.com>
2019-03-21 08:48:04 +0100

    227
    styling

--- todo2/assets/js/main.js
+++ todo2/assets/js/main.js
@@ -1,16 +1,27 @@
     //check off specific item by clicking
-    $("li").click(function(){ 
+    $("ul").on("click","li", function(){ 
       $(this).toggleClass("completed");
-    })
+    });
 
     //click on X to delet todo
-    $("span").click(function(){
+    $("ul").on("click", "span", function(event){
       $(this).parent().fadeOut(500, function(){
         $(this).remove();
       });
       event.stopPropagation();
-    })
+    });
     
+    $("input[type='text']").keypress(function(event){
+      //which === 13(because enter keycode is 13
+      if(event.which === 13){
+      //grabbing new todo text from input
+        var todoText = $(this).val();
+      //clear the input after enter key
+      $(this).val("");
+      //create a new li and add to ul
+      $("ul").append("<li><span><i class='far fa-trash-alt'></i></span>"+ todoText+ "</li>");
+      }
+    });
 
 
 
